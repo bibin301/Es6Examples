b19f00ea6e68ea18eaff4002971007ff3a3ff0dc
bibin <bibin301@gmail.com>
2017-05-04 00:22:40 +0530

    Update session_storage.js
    page redirection

--- session_storage.js
+++ session_storage.js
@@ -9,6 +9,33 @@ var appObj ={
 };
 
 
+var success = function(response){
+	
+	localStorage.setItem('auth_token',response.data.auth_token);
+	$location.path('/dashboard');
+}
+
+.factory('authIntercepter',function($q,$location){
+	return{
+		request:function(config){
+			config.headers = config.headers ||{};
+			if(localStorage.auth_token){
+				config.headers.token = localStorage.auth_token;
+			}
+			return config;
+		},
+		responseError:function(response){
+			if(response.status===401){
+				$location.path('/login');
+				return $q.reject(response);
+			}
+		}
+	})
+	
+	.config(function($httpProvider){
+		$httpProvider.intercepters.push('authIntercepter');
+	})
+
 // Code goes here
 
 /*  App creation:
